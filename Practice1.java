b7cfd1d836cc241fa979265cdcf58233e65ea3cc
michimichi2021 <michikaho1009@gmail.com>
2021-11-16 03:04:31 +0900

    第1章

---
+++ Practice1.java
@@ -0,0 +1,22 @@
+public class Practice1 {
+  public static void main(String[] args){
+    int age = 20;
+    System.out.println("私の年齢は"+age);
+    age = 31;
+    System.out.println("....いや、本当の年齢は"+age);
+    final double PI = 3.14;
+    int pie = 5;
+    System.out.println("半径"+pie+"cmのパイの面積は、");
+    System.out.println(pie*pie*PI);
+    System.out.println("パイの半径を倍にします");
+    int a = 3;
+    int b = 5;
+    int c = a*b;
+    System.out.println("縦幅"+a+"横幅"+b+"の長方形の面積は、"+c);
+    boolean yes = true;
+    char word = '駆';
+    double pai = 3.14;
+    long number = 314159265853979L;
+    String message = "ミナトの攻撃!敵に15ポイントのダメージを与えた。";
+  }
+}
