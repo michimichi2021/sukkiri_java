43abf2f123fb8f674aa8a519190a823e1bb482ed
michimichi2021 <michikaho1009@gmail.com>
2021-11-18 03:52:13 +0900

    第2章

---
+++ Practice2.java
@@ -0,0 +1,28 @@
+public class Practice2{
+  public static void main(String[] args){
+    System.out.println("私の好きな記号は二重引用符(\")です");
+    int a;
+    a=100;
+    a++;
+    System.out.println(a);
+    float f=3;
+    double d=f;
+    System.out.println(f);
+    System.out.println(d);
+    int age = (int)3.2;
+    System.out.println(age);
+    double g =8.5/2;
+    long h = 5+2L;
+    System.out.println(g);
+    System.out.println(h);
+    String msg ="私の年齢は"+23;
+    System.out.println(msg);
+    int i =5;
+    int j =10;
+    int m = Math.max(i,j);
+    System.out.println(m);
+    String ageplus="31";
+    int n =Integer.parseInt(ageplus);
+    System.out.println(ageplus+2);
+  }
+}
