19c297e5251080134ee5b4a1e34faa7434bd62f9
michimichi2021 <michikaho1009@gmail.com>
2021-11-18 18:19:42 +0900

    第2章練習問題終了

--- Practice2.java
+++ Practice2.java
@@ -24,5 +24,27 @@ public class Practice2{
     String ageplus="31";
     int n =Integer.parseInt(ageplus);
     System.out.println(ageplus+2);
+    // int r=new java.util.Random().nextInt(90);
+    // System.out.println("あなたはたぶん、"+r+"歳ですね?");
+    // System.out.println("あなたの名前を入力してください。");
+    // String name= new java.util.Scanner(System.in).nextLine();
+    // System.out.println("あなたの年齢を入力してください。");
+    // int s= new java.util.Scanner(System.in).nextInt();
+    // System.out.println("ようこそ"+s+"歳の"+name+"さん");
+    int x=5;
+    int y=10;
+    String ans ="x+yは"+(x+y);
+    System.out.println(ans);
+    System.out.println("ようこそ占いの館へ");
+    System.out.println("あなたの名前を入力してください");
+    String name=new java.util.Scanner(System.in).nextLine();
+    System.out.println("あなたの年齢を入力してください。");
+    String ageString=new java.util.Scanner(System.in).nextLine();
+    int v =Integer.parseInt(ageString);
+    int fortune=new java.util.Random().nextInt(4);
+    fortune++;
+    System.out.println("占いの結果が出ました！");
+    System.out.println(v+"歳の"+name+"さん、あなたの運気番号は"+fortune+"です");
+    System.out.println("1:大吉　2:中吉 3:吉 4:凶");
   }
 }
